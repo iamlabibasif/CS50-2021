2d776fbd136a39779e95def221ab81ee47246751
iamlabibasif <iamlabibasif5@gmail.com>
2022-05-24 20:15:46 +0500

    Lab 2 - Scrabble

---
+++ Lab 2/scrabble.c
@@ -0,0 +1,64 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+
+    else if (score1 == score2)
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for
+
+    //Storing each letter in the array
+    char letters [26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    int count = 0;
+
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        //converting each letter to capital
+        word [i] = toupper(word [i]);
+
+        //running this loop 27 times as there are 27 letters
+        for (int j = 0; j <= 26; j++)
+        {
+            //Calculating at which index the current index is equal too
+            if (word [i] == letters [j])
+            {
+                //now taking the points from that index from the POINTS array
+                count = count + POINTS [j];
+            }
+        }
+    }
+    return count;
+}
